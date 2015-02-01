1f2e394d7a2cac53d66816880740ebf83bcba76f
Patrick-Wang <wangx229@gmail.com>
2015-02-02 01:01:04 +0800

    更新版本号和TODO

--- Src/BlueLightSPS/Blue/CommonDefine.h
+++ Src/BlueLightSPS/Blue/CommonDefine.h
@@ -4,7 +4,7 @@ extern CString g_strHostName;
 
 #define IDS_HOST_NAME             (LPCTSTR)g_strHostName
 
-#define IDS_VERSION_NUMBER	 _T("1.0.0.01250")
+#define IDS_VERSION_NUMBER	 _T("1.0.0.02010")
 //user setting location
 
 #define IDS_SETTING_FILENAME				_T("\\setting.xml")
