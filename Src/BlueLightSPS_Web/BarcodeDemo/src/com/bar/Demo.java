1f45f3bea9092f76714a8eaaf5a3594fa728ac72
FloydSun <sun.sunshine@163.com>
2015-04-08 22:27:29 +0800

    update demo

---
+++ Src/BlueLightSPS_Web/BarcodeDemo/src/com/bar/Demo.java
@@ -0,0 +1,61 @@
+package com.bar;
+
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import org.jbarcode.JBarcode;
+import org.jbarcode.encode.Code39Encoder;
+import org.jbarcode.encode.EAN13Encoder;
+import org.jbarcode.paint.BaseLineTextPainter;
+import org.jbarcode.paint.EAN13TextPainter;
+import org.jbarcode.paint.WideRatioCodedPainter;
+import org.jbarcode.paint.WidthCodedPainter;
+import org.jbarcode.util.ImageUtil;
+
+public class Demo {
+	public static void main(String[] paramArrayOfString) {
+		try {
+			JBarcode localJBarcode = new JBarcode(EAN13Encoder.getInstance(),
+					WidthCodedPainter.getInstance(),
+					EAN13TextPainter.getInstance());
+			// 条形码数值
+			String str = "552885233512";
+			BufferedImage localBufferedImage = localJBarcode.createBarcode(str);
+			saveToGIF(localBufferedImage, "EAN13.gif");
+			// 条形码值
+			str = "JBARCODE-39";
+			localJBarcode.setEncoder(Code39Encoder.getInstance());
+			localJBarcode.setPainter(WideRatioCodedPainter.getInstance());
+			localJBarcode.setTextPainter(BaseLineTextPainter.getInstance());
+			localJBarcode.setShowCheckDigit(false);
+			localBufferedImage = localJBarcode.createBarcode(str);
+			saveToPNG(localBufferedImage, "Code39.png");
+		} catch (Exception localException) {
+			localException.printStackTrace();
+		}
+	}
+
+	static void saveToJPEG(BufferedImage paramBufferedImage, String paramString) {
+		saveToFile(paramBufferedImage, paramString, "jpeg");
+	}
+
+	static void saveToPNG(BufferedImage paramBufferedImage, String paramString) {
+		saveToFile(paramBufferedImage, paramString, "png");
+	}
+
+	static void saveToGIF(BufferedImage paramBufferedImage, String paramString) {
+		saveToFile(paramBufferedImage, paramString, "gif");
+	}
+
+	static void saveToFile(BufferedImage paramBufferedImage,
+			String paramString1, String paramString2) {
+		try {
+			FileOutputStream localFileOutputStream = new FileOutputStream(
+					"I:/" + paramString1);
+			ImageUtil.encodeAndWrite(paramBufferedImage, paramString2,
+					localFileOutputStream, 96, 96);
+			localFileOutputStream.close();
+		} catch (Exception localException) {
+			localException.printStackTrace();
+		}
+	}
+}
