cd2e38cac7fed0edf9081fcb1121bf67c0fab69a
FloydSun <sun.sunshine@163.com>
2015-06-11 16:38:22 +0800

    添加条形码生成Demo

---
+++ Src/BlueLightSPS_Web/ExcelDemo/VBARunner/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VBARunner
+{
+    public class Cell
+    {
+        public int sheet;
+        public int row;
+        public int col;
+    }
+}
---
+++ Src/BlueLightSPS_Web/ExcelDemo/WindowsFormsApplication3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
+
+namespace WindowsFormsApplication3
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Microsoft.Office.Interop.Excel.Application app = new Microsoft.Office.Interop.Excel.Application();
+            //Workbook book = app.Workbooks.Add("E:\\12.xls");
+            //Sheets shs = book.Sheets;
+
+            //for(int i = 0; i < shs.Count; ++i){
+            //    _Worksheet _wsh = (_Worksheet)shs.get_Item(i + 1);
+            //    Range test = (Range)_wsh.Cells[3, 3];
+            //    String val = test.Value;
+            //    test.Value = val;
+            //    int k = 0;
+            //}
+            VBARunner.VBAExcel.Start();
+
+            VBARunner.Cell[] cells = {
+                new VBARunner.Cell(),
+                new VBARunner.Cell(),
+                new VBARunner.Cell(),
+                new VBARunner.Cell(),
+                new VBARunner.Cell(),
+                new VBARunner.Cell()
+            };
+
+            cells[0].sheet = 0;
+            cells[0].row = 2;
+            cells[0].col = 2;
+
+            cells[1].sheet = 1;
+            cells[1].row = 2;
+            cells[1].col = 2;
+
+            cells[2].sheet = 2;
+            cells[2].row = 2;
+            cells[2].col = 2;
+
+            cells[3].sheet = 3;
+            cells[3].row = 2;
+            cells[3].col = 2;
+
+            VBARunner.VBAExcel.RunBarcodeVBA("E:\\12.xls", cells);
+
+            VBARunner.VBAExcel.Stop();
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+            System.Windows.Forms.Application.Run(new Form1());
+        }
+    }
+}
